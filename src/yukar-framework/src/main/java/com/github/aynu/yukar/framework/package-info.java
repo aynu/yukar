6b7c8cc6f71fe14e02d55158c80d5dea68818df1
nilcy <nilcy@mac.com>
2014-10-09 15:04:53 +0900

    1: Mavenプロジェクトを方向付ける。
    Task-Url: https://github.com/aynu/yukar/issues/issues/issue/1
    
    Signed-off-by: nilcy <nilcy@mac.com>

--- src/yukar-framework/src/main/java/com/github/aynu/yukar/framework/package-info.java
+++ src/yukar-framework/src/main/java/com/github/aynu/yukar/framework/package-info.java
@@ -1,12 +1,13 @@
 // ----------------------------------------------------------------------------
-// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// Copyright (C) Yukar Evolution Laboratory. All rights reserved.
 // GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 // http://www.gnu.org/licenses/gpl-3.0-standalone.html
 // ----------------------------------------------------------------------------
 /**
+ * フレームワーク
  * <dl>
  * <dt>使用条件
- * <dd>TODO 各操作前後の不変条件を表明すること。
+ * <dd>ドメイン共通のアプリケーション骨格として使用すること。
  * </dl>
  * @author nilcy
  */
