7752564fdf7967b52e422161294c9bfccfabc414
nilcy <nilcy@mac.com>
2014-10-17 15:39:21 +0900

    1: Mavenプロジェクトを方向付ける。
    Task-Url: https://github.com/aynu/yukar/issues/issues/issue/1
    
    Signed-off-by: nilcy <nilcy@mac.com>

--- src/yukar-framework/src/test/java/com/github/aynu/yukar/framework/component/SecurityHelperTest.java
+++ src/yukar-framework/src/test/java/com/github/aynu/yukar/framework/component/SecurityHelperTest.java
@@ -6,15 +6,19 @@
 package com.github.aynu.yukar.framework.component;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import org.junit.Test;
-import com.github.aynu.yukar.framework.component.SecurityHelper;
 /**
  * @see SecurityHelper
  * @author nilcy
  */
-@SuppressWarnings({ "static-method", "boxing" })
+@SuppressWarnings({ "static-method", "boxing", "javadoc" })
 public class SecurityHelperTest {
     /**
      * @throws NoSuchAlgorithmException 該当アルゴリズムなし例外
@@ -27,5 +31,34 @@ public class SecurityHelperTest {
         for (int i = 0; i < 1000; ++i) {
             assertThat(testee.nextDouble(), is(closeTo(0, 1.0)));
         }
+    }
+    @Test
+    public void test() {
+        final KeyPair keyPair = SecurityHelper.createKeyPair();
+        assertThat(keyPair, is(not(nullValue())));
+        final RSAPublicKey publicKey = SecurityHelper.createPublicKey(
+            ((RSAPublicKey) keyPair.getPublic()).getModulus(),
+            ((RSAPublicKey) keyPair.getPublic()).getPublicExponent());
+        final RSAPrivateKey privateKey = SecurityHelper.createPrivateKey(
+            ((RSAPrivateKey) keyPair.getPrivate()).getModulus(),
+            ((RSAPrivateKey) keyPair.getPrivate()).getPrivateExponent());
+        assertThat(publicKey.getEncoded(), is(keyPair.getPublic().getEncoded()));
+        final SecretKey secretKey = SecurityHelper.createSecretKey("hello, world!!".toCharArray(),
+            new byte[] { 1 });
+        assertThat(secretKey, is(not(nullValue())));
+        final byte[] encrypted = SecurityHelper.encrypt(publicKey, secretKey.getEncoded());
+        final byte[] decrypted = SecurityHelper.decrypt(privateKey, encrypted);
+        assertThat(decrypted, is(secretKey.getEncoded()));
+        // ----
+        final KeyPair signKeyPair = SecurityHelper.createSignKeyPair();
+        final byte[] sign = SecurityHelper.sign(signKeyPair.getPrivate(), "DATA".getBytes());
+        assertThat(SecurityHelper.verify(signKeyPair.getPublic(), sign, "DATA".getBytes()),
+            is(true));
+        // ----
+        final SecretKey secretKey2 = SecurityHelper.createSecretKey("1234567890123456".getBytes());
+        final IvParameterSpec iv = new IvParameterSpec("abcdefghijklmnop".getBytes());
+        final byte[] encrypted2 = SecurityHelper.encrypt(secretKey2, iv, "BIGDATA".getBytes());
+        final byte[] decrypted2 = SecurityHelper.decrypt(secretKey2, iv, encrypted2);
+        assertThat(decrypted2, is("BIGDATA".getBytes()));
     }
 }
