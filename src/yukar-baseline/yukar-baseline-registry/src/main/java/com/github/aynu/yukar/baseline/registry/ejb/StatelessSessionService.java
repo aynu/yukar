a9f2e3f050f8759594e922636c44ff7daa0d1316
nilcy <nilcy@mac.com>
2014-10-22 15:52:09 +0900

    1: Mavenプロジェクトを方向付ける。
    Task-Url: https://github.com/aynu/yukar/issues/issues/issue/1
    
    Signed-off-by: nilcy <nilcy@mac.com>

--- src/yukar-baseline/yukar-baseline-registry/src/main/java/com/github/aynu/yukar/baseline/registry/ejb/StatelessSessionService.java
+++ src/yukar-baseline/yukar-baseline-registry/src/main/java/com/github/aynu/yukar/baseline/registry/ejb/StatelessSessionService.java
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------
-// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// Copyright (C) Yukar Evolution Laboratory. All rights reserved.
 // GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 // http://www.gnu.org/licenses/gpl-3.0-standalone.html
 // ----------------------------------------------------------------------------
